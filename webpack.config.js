073ae919270463a7d9ec73d7a124c7bed55519c5
Ashley Gibson <ashley@nosegraze.com>
2021-09-01 13:41:48 +0100

    Rework webpack to use WordPress scripts #22

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+const IgnoreEmitWebpackPlugin = require( 'ignore-emit-webpack-plugin' );
+
+/**
+ * WordPress dependencies.
+ */
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+const cwd = process.cwd();
+
+module.exports = {
+	...defaultConfig,
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM',
+	},
+	entry: {
+		// Scripts.
+		'admin-scripts': path.resolve( cwd, 'assets/js', 'admin-scripts.js' ),
+		'jquery.recopy': path.resolve( cwd, 'assets/js', 'jquery.recopy.js' ),
+		'media-upload': path.resolve( cwd, 'assets/js', 'media-upload.js' ),
+		'widget-settings': path.resolve( cwd, 'assets/js', 'widget-settings.js' ),
+
+		// Styles.
+		// Prefix all entry points with `style-` to ensure style-only entry points
+		// do not generate extraneous .js files.
+		'style-admin': path.resolve( cwd, 'assets/css', 'novelist-admin.scss' ),
+	},
+	output: {
+		...defaultConfig.output,
+		path: path.resolve( cwd, 'assets/build' ),
+	},
+	optimization: {
+		...defaultConfig.optimization,
+		// Disable automatic splitting of modules in to separate files when
+		// CSS is detected. CSS is loaded directly through entry points instead
+		// of imported within a JS module.
+		//
+		// https://github.com/WordPress/gutenberg/blob/trunk/packages/scripts/config/webpack.config.js#L114-L119
+		splitChunks: {
+			...defaultConfig.optimization.splitChunks,
+			cacheGroups: {
+				...defaultConfig.optimization.splitChunks.cacheGroups,
+				style: {},
+			},
+		},
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		// Ignore files starting with `style-` from being emitted.
+		new IgnoreEmitWebpackPlugin( /style-(.*).js/ ),
+	],
+};
