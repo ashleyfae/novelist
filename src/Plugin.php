b60b90836e445e0f4424cf12346b7399f603154f
Ashley Gibson <ashley@nosegraze.com>
2021-11-21 13:42:58 +0000

    Rework main plugin class & add LegacyServiceProvider.

---
+++ src/Plugin.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Plugin.php
+ *
+ * @package   novelist
+ * @copyright Copyright (c) 2021, Ashley Gibson
+ * @license   GPL2+
+ * @since     2.0
+ */
+
+namespace Novelist;
+
+use Novelist\ServiceProviders\LegacyServiceProvider;
+
+/**
+ * @since 2.0 Class renamed.
+ */
+class Plugin
+{
+
+    /** @var Container\Container */
+    private $container;
+
+    /**
+     * Service providers to boot.
+     *
+     * @see Novelist::boot()
+     * @see Novelist::loadServiceProviders()
+     *
+     * @var string[]
+     */
+    private $serviceProviders = [
+        LegacyServiceProvider::class,
+    ];
+
+    /** @var bool */
+    private $serviceProvidersLoaded = false;
+
+    public function __construct()
+    {
+        $this->container = new Container\Container();
+    }
+
+    public function boot(): void
+    {
+        $this->setupConstants();
+        $this->loadServiceProviders();
+
+        add_action('plugins_loaded', [$this, 'load_textdomain']);
+    }
+
+    /**
+     * Novelist instance.
+     *
+     * Insures that only one instance of Novelist exists at any one time.
+     *
+     * @deprecated 2.0
+     *
+     * @access public
+     * @since  1.0.0
+     * @return Plugin Instance of Novelist class
+     */
+    public static function instance(): Plugin
+    {
+        return novelist();
+    }
+
+    /**
+     * Properties are loaded from the service container.
+     *
+     * @since 2.0
+     *
+     * @param  string  $property
+     *
+     * @return mixed|object
+     * @throws \Exception
+     */
+    public function __get($property)
+    {
+        return $this->container->get($property);
+    }
+
+    /**
+     * Magic methods are passed to the service container.
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->container, $name], $arguments);
+    }
+
+    private function setupConstants(): void
+    {
+        // Plugin Folder Path.
+        if (! defined('NOVELIST_PLUGIN_DIR')) {
+            define('NOVELIST_PLUGIN_DIR', plugin_dir_path(NOVELIST_PLUGIN_FILE));
+        }
+
+        // Plugin Folder URL.
+        if (! defined('NOVELIST_PLUGIN_URL')) {
+            define('NOVELIST_PLUGIN_URL', plugin_dir_url(NOVELIST_PLUGIN_FILE));
+        }
+    }
+
+    /**
+     * Registers and boots all service providers.
+     *
+     * @since 2.0
+     */
+    private function loadServiceProviders()
+    {
+        if ($this->serviceProvidersLoaded) {
+            return;
+        }
+
+        $providers = [];
+        foreach ($this->serviceProviders as $serviceProvider) {
+            if (! is_subclass_of($serviceProvider, ServiceProviders\ServiceProvider::class)) {
+                throw new \InvalidArgumentException(sprintf(
+                    '%s class must implement the ServiceProvider interface.',
+                    $serviceProvider
+                ));
+            }
+
+            /** @var ServiceProviders\ServiceProvider $serviceProvider */
+            $serviceProvider = new $serviceProvider();
+            $serviceProvider->register();
+            $providers[] = $serviceProvider;
+        }
+
+        foreach ($providers as $serviceProvider) {
+            $serviceProvider->boot();
+        }
+
+        $this->serviceProvidersLoaded = true;
+    }
+
+    /**
+     * Loads the plugin language files.
+     *
+     * @access public
+     * @since  1.0.0
+     * @return void
+     */
+    public function load_textdomain(): void
+    {
+        $lang_dir = dirname(plugin_basename(NOVELIST_PLUGIN_FILE)).'/languages/';
+        $lang_dir = apply_filters('novelist/languages-directory', $lang_dir);
+        load_plugin_textdomain('novelist', false, $lang_dir);
+    }
+
+}
---
+++ src/ServiceProviders/LegacyServiceProvider.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * LegacyServiceProvider.php
+ *
+ * @package   novelist
+ * @copyright Copyright (c) 2021, Ashley Gibson
+ * @license   GPL2+
+ */
+
+namespace Novelist\ServiceProviders;
+
+use Novelist\Plugin;
+
+class LegacyServiceProvider implements ServiceProvider
+{
+
+    public function register(): void
+    {
+        $this->includeFiles();
+        $this->registerClassAliases();
+        $this->bindClasses();
+    }
+
+    public function boot(): void
+    {
+
+    }
+
+    private function includeFiles(): void
+    {
+        global $novelist_options;
+
+        // Settings.
+        require_once NOVELIST_PLUGIN_DIR.'includes/admin/settings/register-settings.php';
+        if (empty($novelist_options)) {
+            $novelist_options = novelist_get_settings();
+        }
+
+        require_once NOVELIST_PLUGIN_DIR.'includes/class-novelist-html.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/class-novelist-roles.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/post-types.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/class-novelist-book.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/class-novelist-shortcodes.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/book-functions.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/book-filters.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/load-assets.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/misc-functions.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/template-functions.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/widgets/widget-book.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/widgets/widget-books-by-series.php';
+        require_once NOVELIST_PLUGIN_DIR.'includes/widgets/widget-word-count.php';
+
+        if (is_admin()) {
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/admin-actions.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/extensions.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/thickbox.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/tools.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/admin-pages.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/class-novelist-notices.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/class-welcome.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/settings/display-settings.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/books/meta-box.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/books/sanitize-meta-fields.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/books/dashboard-columns.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/books/demo-book.php';
+            require_once NOVELIST_PLUGIN_DIR.'includes/admin/upgrades/upgrade-functions.php';
+        }
+
+        require_once NOVELIST_PLUGIN_DIR.'includes/install.php';
+    }
+
+    private function registerClassAliases(): void
+    {
+        class_alias(Plugin::class, 'Novelist');
+    }
+
+    /**
+     * Binds classes that used to be properties in the `Novelist` class. (now `Novelist\Plugin`)
+     */
+    private function bindClasses(): void
+    {
+        novelist()->bind(\Novelist_Roles::class);
+        novelist()->alias(\Novelist_Roles::class, 'roles');
+
+        novelist()->bind(\Novelist_HTML::class);
+        novelist()->alias(\Novelist_HTML::class, 'html');
+    }
+}
